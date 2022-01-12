c0c40eebb839728dd711f02a62b701158fb31098
Kyle Locquiao <klocquiao45@gmail.com>
2022-01-11 23:13:01 -0800

    Use server setup from Boost documentation, intialize copyright .txt, and add comments.

--- server/server.cpp
+++ server/server.cpp
@@ -1,7 +1,133 @@
-#include <server.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind/bind.hpp>
+#include <boost/asio.hpp>
 
-int main()
+using boost::asio::ip::tcp;
+
+class session
 {
+public:
+  session(boost::asio::io_context& io_context)
+    : socket_(io_context)
+  {
+  }
 
-    Socket socket;
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
+
+  void start()
+  {
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        boost::bind(&session::handle_read, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  void handle_read(const boost::system::error_code& error,
+      size_t bytes_transferred)
+  {
+    if (!error)
+    {
+      boost::asio::async_write(socket_,
+          boost::asio::buffer(data_, bytes_transferred),
+          boost::bind(&session::handle_write, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  void handle_write(const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      socket_.async_read_some(boost::asio::buffer(data_, max_length),
+          boost::bind(&session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+class server
+{
+public:
+  
+  //constructor initializes an "acceptor" that listens for TCP on a specified port
+  server(boost::asio::io_context& io_context, short port)
+    : io_context_(io_context),
+      acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+  {
+    //when acceptor receives a transmission, it will start a session with the sender?
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    session* new_session = new session(io_context_);
+    acceptor_.async_accept(new_session->socket(),
+        boost::bind(&server::handle_accept, this, new_session,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(session* new_session,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      new_session->start();
+    }
+    else
+    {
+      delete new_session;
+    }
+
+    start_accept();
+  }
+
+  boost::asio::io_context& io_context_;
+  tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_context io_context;
+
+    using namespace std; // For atoi.
+
+    ///this server object is created to accept incoming client connections
+    server s(io_context, atoi(argv[1]));
+
+    //we will run the server object to perform async operations
+    io_context.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }
